c7bc6ac3368dda8c0a6ae761b3baf060fd372d4f
macbook <macbook@macbook-Lenovo-B40-80>
2019-03-18 23:53:15 +0530

    PendingIntent

--- NotificationDemo/gen/com/example/notificationdemo/R.java
+++ NotificationDemo/gen/com/example/notificationdemo/R.java
@@ -1195,10 +1195,12 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_second=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
+        public static final int second=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1244,6 +1246,7 @@ containing a value of this type.
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_second=0x7f0a0010;
     }
     public static final class style {
         /** 
--- NotificationDemo/src/com/example/notificationdemo/MainActivity.java
+++ NotificationDemo/src/com/example/notificationdemo/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.NotificationCompat.Builder;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,8 +23,16 @@ public class MainActivity extends ActionBarActivity {
     public void getNotify(View v) {
 		NotificationCompat.Builder builder=new NotificationCompat.Builder(this);
 		builder.setContentTitle("Notification");
-		builder.setContentText("this demo notification");
+		builder.setContentText("This demo notification");
 		builder.setSmallIcon(R.drawable.ic_launcher);
+		builder.setAutoCancel(true);
+		
+		//PendingIntent
+		Intent intent=new Intent(this, SecondActivity.class);
+		intent.putExtra("msg", "This is a action message");
+		PendingIntent pendingIntent=PendingIntent.getActivity(this, 0, intent, 0);
+		builder.setContentIntent(pendingIntent);
+		
 		Notification notification=builder.build();
 		NotificationManager notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 		notificationManager.notify(1, notification);
